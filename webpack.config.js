e967f1f259a2bbc1cc495e19ceade7d49c4ee52f
Carlos Dias <carlosdias@rainpos.com>
2023-08-30 14:08:47 +0200

    Webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+// webpack.config.js
+module.exports = {
+  resolve: {
+    alias: {
+      'vue$': 'vue@compat'
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          compilerOptions: {
+            compatConfig: {
+              MODE: 2
+            }
+          }
+        }
+      }
+    ]
+  }
+}
